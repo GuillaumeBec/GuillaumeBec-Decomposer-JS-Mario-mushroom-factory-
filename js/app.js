5bd235016818e2860738a858ae5b1264043c53d2
GuillaumeBec <etudiant@teleporteur.oclock>
2020-06-09 17:22:59 +0200

    Traçage du table

--- js/app.js
+++ js/app.js
@@ -1,26 +1,42 @@
 var app = {
-  init: function() {
-    console.log('init');
+  init: function () {
+    console.log("init");
 
     // TODO
     app.drawBoard();
 
     // Event listeners - TODO
   },
-  handleLaunchScriptButton: function() {
+  drawBoard: function () {
+    let container = document.querySelector("#board");
+
+    console.log(container);
+
+    for (let i = 0; i < 4; i++) {
+      let cellRow = document.createElement("div");
+      cellRow.classList.add("cellRow");
+      container.appendChild(cellRow);
+      for (let j = 0; j <= 5; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cellRow.appendChild(cell);
+        //console.log(cell);
+      }
+    }
+  },
+  handleLaunchScriptButton: function () {
     // TODO
-    
+
     // TODO : get all lines as an array
 
-    window.setTimeout(function() {
+    window.setTimeout(function () {
       app.codeLineLoop(codeLines, 0);
     }, 2000);
   },
-  codeLineLoop: function(codeLines, index) {
+  codeLineLoop: function (codeLines, index) {
     // Getting currentLine
     var currentLine = codeLines[index];
     console.log(currentLine);
-
 
     // Increment
     index++;
@@ -28,18 +44,18 @@ var app = {
     // if still a line to interpret
     if (index < codeLines.length) {
       // Recall same method (=> make a loop)
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         app.codeLineLoop(codeLines, index);
       }, 1000);
     } else {
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         app.checkSuccess();
       }, 1000);
     }
   },
-  checkSuccess: function() {
+  checkSuccess: function () {
     // TODO display if the game is won or not
-  }
+  },
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
+document.addEventListener("DOMContentLoaded", app.init);
