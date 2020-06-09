209b562b92c329c083f933345da9d8a6f53e19b3
GuillaumeBec <etudiant@teleporteur.oclock>
2020-06-09 19:07:35 +0200

    regroupement des cells et reconnaissance sens de la flèche

--- js/app.js
+++ js/app.js
@@ -3,19 +3,21 @@ var app = {
     console.log("init");
 
     // TODO
-    app.drawBoard();
 
+    app.drawBoard();
+    app.moveForward();
     // Event listeners - TODO
   },
   drawBoard: function () {
     let container = document.querySelector("#board");
 
-    console.log(container);
+    //console.log(container);
 
     //! 1) on dessine le tableau
     for (let i = 0; i < 4; i++) {
       let cellRow = document.createElement("div");
       cellRow.classList.add("cellRow");
+      cellRow.id = "row" + (i + 1);
       container.appendChild(cellRow);
       for (let j = 0; j <= 5; j++) {
         let cell = document.createElement("div");
@@ -29,10 +31,41 @@ var app = {
     let cells = document.querySelectorAll(".cell");
     let startCell = cells[0];
     startCell.classList.add("cellStart");
-    console.log(cells.length);
+    //! 3) le curseur commence ici
+    startCell.classList.add("cellCurrent");
+    //console.log(cells.length);
     let endCell = cells[cells.length - 1];
     endCell.classList.add("cellEnd");
   },
+
+  moveForward: function () {
+    //! 1) Collectionnons les cellules dans une tableau pour pouvoir naviguer de l'une à l'autre
+    let currentCell = document.querySelector(".cellCurrent");
+    let cells = document.querySelectorAll(".cell");
+
+    let rankedCell = [];
+
+    for (let cell in cells) {
+      rankedCell.push(cell);
+    }
+
+    //! 2) Par défaut, le curseur à la class -right dans la cellule de départ
+    currentCell.classList.add("cellCurrent-right");
+    console.log(currentCell);
+    console.log(currentCell.classList.contains("cellCurrent"));
+    console.log(currentCell.classList);
+
+    if (currentCell.classList.contains("cellCurrent-top")) {
+      console.log("on est au top");
+    } else if (currentCell.classList.contains("cellCurrent-bottom")) {
+      console.log("la tête en bas");
+    } else if (currentCell.classList.contains("cellCurrent-left")) {
+      console.log("virage à gauche");
+    } else if (currentCell.classList.contains("cellCurrent-right")) {
+      console.log("virage à droite");
+    }
+  },
+
   handleLaunchScriptButton: function () {
     // TODO
 
